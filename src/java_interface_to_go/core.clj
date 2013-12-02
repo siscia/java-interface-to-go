a4fe4d96c27cce1abe363c1d3dd78e191ad6d1b4
Simone "siscia" Mosciatti <simone@mweb.biz>
2013-12-02 23:47:53 +0100

    now make-file interfaces print only the necessary

--- src/java_interface_to_go/core.clj
+++ src/java_interface_to_go/core.clj
@@ -1,14 +1,14 @@
 (ns java-interface-to-go.core
   (:use [clojure.tools.cli :only [cli]])
   (:use [clojure.java.io :only [writer]])
-  (:use [java-interface-to-go.from-runtime :only [make-tree]]))
+  (:use [java-interface-to-go.from-runtime :only [make-tree]]
+        )
+  (:use [clojure.set :only [difference]]))
 
 ;; software to be run as command line, it takes in input a string, and it evals the class it represent:
 ;; (class (eval input-str))
 ;; it is dangerous but it is suppose to be used only to generate other source file it stilll should be fine.
 ;; then the it spit the go interfaces.
-
-
 
 (defn capitalize-first-only [s]
   (when (> (count s) 0)
@@ -56,25 +56,39 @@
                         (capitalize-first-only (:method-name meth))
                         "(" parameter-string ")" "\n")))))
 
-(defn write-interface [super-class interface meth wrtr]
-  (.write wrtr (str "\n" "type Interface interface{" "\n"))
-  (write-import-super-class super-class wrtr)
-  (write-import-extended-interface interface wrtr)
-  (write-methods meth wrtr)
-  (.write wrtr (str "}" "\n")))
+(defn write-interface
+  ([super-class interface meth wrtr]
+     (.write wrtr (str "\n" "type Interface interface{" "\n"))
+     (write-import-super-class super-class wrtr)
+     (write-import-extended-interface interface wrtr)
+     (when meth
+       (do (println meth)
+           (write-methods meth wrtr)))
+     (.write wrtr (str "}" "\n")))
+  ([]
+     nil))
 
 (defn make-file [source dir]
   (let [file-name (-> (str dir (:name source) ".go")
-                      clojure.string/lower-case)]
+                      clojure.string/lower-case)
+        interfaces (map make-tree (:interface source))
+        interface-meth (map :methods interfaces)
+        super-class (make-tree (:super-class source))
+        super-class-meth (:methods super-class)
+        already-define-meth (set (concat interface-meth super-class-meth))]
     (with-open [wrtr (writer file-name)]
-      (println "prova")
       (write-name (:name source) wrtr)
       (write-extended (:super-class source) (:interface source) wrtr)
       (write-interface (:super-class source)
                        (:interface source)
-                       (:methods source)
+                       (when-let [meth (difference (remove nil? (set (:methods source)))
+                                                   (remove nil? already-define-meth)
+                                                   
+                                                   )]
+                         meth)
                        wrtr)
-      (println file-name))
+      (.write wrtr "\n"))
+    (println file-name)
     file-name))
 
 (defn arguments-receiver [args]
@@ -84,5 +98,4 @@
 (defn -main [input & args]
   (let [[options args banner] (arguments-receiver args)
         tree (make-tree (class [:a]))]
-    (println (options :output))
     (make-file (first tree) (options :output))))
